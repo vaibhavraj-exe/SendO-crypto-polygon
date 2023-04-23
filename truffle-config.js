2737d5afaba94d27eb4bf3f2a38808d8c3827b21
Kshitij Mishra <kshitijmishra1302@gmail.com>
2023-04-24 01:18:31 +0530

    Code Improvements

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost
-      port: 7545,            // Ganache port
-      network_id: "*",      // Match any network ID
+      host: "127.0.0.1",     
+      port: 7545,            
+      network_id: "*",      
     },
   },
   compilers: {
     solc: {
-      version: "0.8.0",     // Compiler version
+      version: "0.8.0",     
     },
   },
 };
